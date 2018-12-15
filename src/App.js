a087adfb121f3b582ac1542e0c71b1b2acd71f67
wonjieun <wonjieunkk@gmail.com>
2018-12-15 15:06:36 +0900

    Header Portals

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 
-import Header from './components/Header';
+// import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Theme from './components/Theme/Theme';
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Header />, document.getElementById('header'));
+  }
+}
+
+const Header = () => {
+  return (
+    <div className="header_bar">
+      <strong className="header_user">관리자님</strong>
+      <button className="header_btn">로그아웃</button>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
+        <Portals />
+        {/* <Header /> */}
         <Navigation />
         <Theme />
         {/* <Notice /> */}
